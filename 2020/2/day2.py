8c2dd581abbb65779c19f5a3550f53860be3ad71
Reid <reid@shipt.com>
2020-12-07 15:30:14 -0600

    adding day 2

--- 2020/2/day2.py
+++ 2020/2/day2.py
@@ -9,24 +9,21 @@ letters =   [x for x in data['letter']]
 minimums =  [x for x in data['min']]
 maximums =  [x for x in data['max']]
 
+# initializing the counts
 count = 0
+count_2 = 0
+
+# looping through the zipped lists (idk how to use pandas tbh)
 for letter, password, mins, maxs in zip(letters, passwords, minimums, maximums):
     letter_count = password.count(letter)
-    # This works
     if letter_count >= mins:
         if letter_count <= maxs:
             count += 1
-    # But this doesn't 
-    # if letter_count >= mins & letter_count <= maxs:
-    # ???
 
-print(count)
-
-count_2 = 0
 for letter, password, idx1, idx2 in zip(letters, passwords, minimums, maximums):
     idx1_exists = password[idx1-1].count(letter)
     idx2_exists = password[idx2-1].count(letter)
     if idx1_exists + idx2_exists == 1:
         count_2 += 1
 
-print(count_2)
+print(f"counts of the first task: {count}\ncounts of the second task: {count_2}")
